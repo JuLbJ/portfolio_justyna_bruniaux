60397162b163d2d1436b8ab5dc50148cc52f3db3
Justyna Bruniaux <justyna.lipska74@wp.pl>
2016-09-23 12:13:00 +0200

    contact.html & header.php ADDED

---
+++ header.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<title>JBL_portfolio | Home</title>
+	<meta name="description" content="Justyna Bruniaux">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="apple-touch-icon" href="apple-touch-icon.png">
+	<!-- ___________________________________________F A V I C O N: before HEAD___-->
+	<link rel="icon" type="img/png" sizes="16x16" href="img/jb_logo1.png">
+	<!-- _________________________________________/ F A V I C O N: before HEAD___-->
+
+<link href="https://fonts.googleapis.com/css?family=Concert+One" rel="stylesheet">
+
+	<link rel="stylesheet" href="css/normalize.min.css">
+	<link rel="stylesheet" href="css/main.css">
+
+	<script src="js/vendor/modernizr-2.8.3.min.js"></script>
+</head>
